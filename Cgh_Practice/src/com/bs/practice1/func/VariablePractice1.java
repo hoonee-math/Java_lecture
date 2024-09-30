656a244468ffefabdce94c1da10abc72f695704d
hoonee-math <kwanghoon041@gmail.com>
2024-09-30 09:02:01 +0900

    [과제] 1_변수

---
+++ Cgh_Practice/src/com/bs/practice1/func/VariablePractice1.java
@@ -0,0 +1,38 @@
+package com.bs.practice1.func;
+
+import java.util.Scanner;
+
+public class VariablePractice1 {
+	public void userInfo() {
+//		 Class Name : com.bs.practice1.func.VariablePractice1
+//		 실행클래스: com.bs.practice1.run.Run
+//		 이름, 성별, 나이, 키를사용자에게입력받아각각의값을변수에담고출력하세요.
+//		  ex.
+//		 이름을입력하세요: 아무개
+//		 성별을입력하세요(남/여) : 남
+//		 나이를입력하세요: 20
+//		 키를입력하세요(cm) : 180.5
+//		 키180.5cm인20살남자아무개님반갑습니다^^
+		
+		Scanner sc=new Scanner(System.in);
+		
+		String name="", gender="";
+		int age=0;
+		double height=0.0;
+		
+		System.out.print("이름을 입력하세요: ");
+		name=sc.next();
+		System.out.print("성별을 입력하세요: ");
+		gender=sc.next();
+		System.out.print("나이를 입력하세요: ");
+		age=sc.nextInt();
+		System.out.print("키를 입력하세요: ");
+		height=sc.nextDouble();
+		
+		System.out.printf("키 %.1f인 %d살 %s자 %s님 반갑습니다^^", height,age, gender, name);
+		
+		
+	}
+
+}
+
---
+++ Cgh_Practice/src/com/bs/practice1/run/Main.java
@@ -0,0 +1,10 @@
+package com.bs.practice1.run;
+
+import com.bs.practice1.func.VariablePractice1;
+
+public class Main {
+	public static void main(String[] args) {
+		new VariablePractice1().userInfo();
+	}
+	
+}
