678da828974a3bad02c5c776b12593dfac261af4
hoonee-math <kwanghoon041@gmail.com>
2024-10-01 23:15:38 +0900

    [ 과제 ] 2. 형변환

--- Cgh_Practice/src/com/bs/practice2/func/CastingPractice2.java
+++ Cgh_Practice/src/com/bs/practice2/func/CastingPractice2.java
@@ -6,6 +6,16 @@ public class CastingPractice2 {
 	public void casting()	{
 		Scanner sc=new Scanner(System.in);
 		
+		double total=0.0, mean=0.0;
+		
+		System.out.print("국어: ");
+		total+=sc.nextDouble();
+		System.out.print("영어: ");
+		total+=sc.nextDouble();
+		System.out.print("수학: ");
+		total+=sc.nextDouble();
+		System.out.println("총점: "+(int)total);
+		System.out.println("평균: "+(int)total/3);
 	}
 
 }
--- Cgh_Practice/src/com/bs/practice2/run/Main.java
+++ Cgh_Practice/src/com/bs/practice2/run/Main.java
@@ -7,14 +7,14 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner sc=new Scanner(System.in);
-		System.out.print("실행시킬 함수를 선택하시오: ");
+		System.out.print("형변환 실습 / 실행시킬 함수를 선택하시오: ");
 		int funcNum=sc.nextInt();
 		System.out.print("======================\n");
 		
 		switch(funcNum){
 			case 1: new CastingPractice1().casting(); break;
-//			case 2:
-//			case 3:
+			case 2: new CastingPractice2().casting(); break;
+			case 3: new CastingPractice3().casting(); break;
 //			case 4:
 //			case 5:
 //			case 6:
