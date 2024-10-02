c50217303450d46680517d5dbd45870693e48c0b
hoonee-math <kwanghoon041@gmail.com>
2024-10-02 09:02:00 +0900

    [ 과제 ] 4.조건문 (진행중)

---
+++ Cgh_Practice/src/com/bs/practice/chap01/Main.java
@@ -0,0 +1,28 @@
+package com.bs.practice.chap01;
+
+import java.util.Scanner;
+import com.bs.practice.chap01.*;
+
+public class Main {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	
+	System.out.print("조건문 실습 / 실행할 함수 번호를 입력하세요: ");
+	int num=sc.nextInt();
+	
+	switch(num) {
+		case 1 : new ControlPractice().practice1(); break;
+		case 2 : new ControlPractice().practice2(); break;
+		case 3 : new ControlPractice().practice3(); break;
+		case 4 : new ControlPractice().practice4(); break;
+		case 5 : new ControlPractice().practice5(); break;
+		case 6 : new ControlPractice().practice6(); break;
+		case 7 : new ControlPractice().practice7(); break;
+		case 8 : new ControlPractice().practice8(); break;
+		case 9 : new ControlPractice().practice9(); break;
+		case 10 : new ControlPractice().practice10(); break;
+		
+		default : System.out.println("유효한 값을 입력하세요!");
+	}
+}
+}
