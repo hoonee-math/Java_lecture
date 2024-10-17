ce07075c85d8bb3b256c18e4f551d1a7de054561
hoonee-math <kwanghoon041@gmail.com>
2024-10-17 14:00:26 +0900

    [ 인터페이스 ] 람다표현식
    1017-6-5
    인터페이스내에 추상메소드를 한개만(디폴트 메소드는 여러개 있어도 상관없음. 스태틱 필드에 여러개있어도 상관없음) 선언하면
     FunctionalInterace 라고 하고, 이때 FunctionalIntercae 는 람다라는 표현식을 이용해서 처리할 수 있음! 13:32 youtube
    사용방법 : (매개변수명)->{로직 작성 [return 반환값]}
    (매개변수명)->{로직 작성}
     그 매개변수명으로 실행되는 것을 표현하기 위해 매개변수 명을 선언
     만약 void 반환 없는 형태이면 이대로 작성가능!
    (매개변수명)->{로직 작성 return 반환값}
     반환형이 있는 경우 중괄호 안에 return 반환값을 써줘야함.

---
+++ 10_인터페이스/src/com/inter/common/D_MyFunctionalInter.java
@@ -0,0 +1,7 @@
+package com.inter.common;
+
+//1017-6-6
+public interface D_MyFunctionalInter {
+	//1017-6-7
+	boolean test(String str); // 이 인터페이스 안에는 현재 추상메소드 이거 하나만 선언되어있음
+}
--- 10_인터페이스/src/com/inter/controller/C_InterController.java
+++ 10_인터페이스/src/com/inter/controller/C_InterController.java
@@ -1,9 +1,12 @@
 package com.inter.controller;
 
+import java.util.List;
+
 import com.inter.common.A_BasicInter;
 import com.inter.common.C_ChildInter;
 import com.inter.common.C_Parent2Inter;
 import com.inter.common.C_ParentInter;
+import com.inter.common.D_MyFunctionalInter;
 import com.inter.common.D_NoNameInter;
 import com.inter.model.vo.A_BasicInterImpl;
 import com.inter.model.vo.C_ChildInterImpl;
@@ -79,5 +82,95 @@ public class C_InterController {
 		//1017-5-12
 		flag=noname.check("가나다라마");
 		
+		//1017-6-2 [1017-6-1] 에서 만든걸 1회용으로 호출해서 사용할 수 있음. testCheck 라는 곳에 전달되면 거기서 .... 13:25 youtube
+		testCheck(new D_NoNameInter() {
+			@Override
+			public boolean check(String str) {
+				return false;
+			}
+			@Override
+			public int length(String str) {
+				return str.length();
+			}
+		});
+		
+		//1017-6-3
+		testCheck(new D_NoNameInter() { // 똑같은 메소드를 실행했지만, 밑에 실제 코드에서 실행하는건 ... 13:27 youtube
+			@Override
+			public boolean check(String str) {
+				return str.equals("test");
+			}
+			@Override
+			public int length(String str) {
+				return str.length();
+			}
+		}); //1017-6-4 메소드가 많은건 어쩔수 없지만(인터페이스 타입에서 check를 호출할지 length 를 호출할지 몰라. 어떤걸 쓸지 명시하고 사용해야함. 인터페이스를 만들때 한나의 추상메소드만 갖고있는 걸 만들 수 잇음. 컴파일 시점에는 메소드를 알 수 있으니까 다음과 같이 사용
+		
+		//1017-6-5
+		//인터페이스내에 추상메소드를 한개만(디폴트 메소드는 여러개 있어도 상관없음. 스태틱 필드에 여러개있어도 상관없음) 선언하면
+		// FunctionalInterace 라고 하고, 이때 FunctionalIntercae 는 람다라는 표현식을 이용해서 처리할 수 있음! 13:32 youtube
+		//사용방법 : (매개변수명)->{로직 작성 [return 반환값]}
+		//(매개변수명)->{로직 작성}
+		// 그 매개변수명으로 실행되는 것을 표현하기 위해 매개변수 명을 선언
+		// 만약 void 반환 없는 형태이면 이대로 작성가능!
+		//(매개변수명)->{로직 작성 return 반환값}
+		// 반환형이 있는 경우 중괄호 안에 return 반환값을 써줘야함.
+		
+		//1017-6-8
+		D_MyFunctionalInter functionalInter;
+		functionalInter = new D_MyFunctionalInter() {
+			@Override
+			public boolean test(String str) {
+				return str.length()	>5;
+			}
+		}; //1017-6-9 표현 내용 불린 반환 매개변수 str 받고 str.length 반환 이걸 표현해보자, 6-8 대신에 6-10 처럼 만들어서 사용할 수 있다!
+		//1017-6-10
+		functionalInter = (content)->{return content.equals("우와신기");};
+		flag = functionalInter.test("우와신기"); // test 는 바로 윗줄 에 구현한걸 말하는 것임. functionalInter 에는 어짜피 추상메소드가 하나만 있으니까 test 라는 메소드명 없이 functionalInter 람다 표현식으로 사용할 수 있음.
+		System.out.println(flag);
+		//1017-6-11
+		functionalInter = (content)->content.equals("우와신기"); // 반환값이 하나면 이렇게도 쓸 수도 있음. 
+		functionalInter = content->content.equals("우와신기"); // 반환값이 하나면 이렇게도 쓸 수도 있음. 
+		//Question 결국 functionalInter 여기에는 추상메소드가 하나만 있으니까 가능하단거지?!
+		//1017-6-13
+		testFunctional(i->i.equals(""));
+		
+		List.of(1,2,3,4,5).stream().filter((i)->i>3).toList(); // (i)->i>3 이 부분의 표현식을 보면, 매개변수를 가져와서 i 를 이용해서.... 크기를 비교해서 boolean 을반환
+	}
+	
+	//1017-6-1
+	public void testCheck(D_NoNameInter no) {
+		System.out.println(no.check("test")); //
+	}
+	
+	//1017-6-14
+	public void testFunctional(D_MyFunctionalInter inter) {
+		inter.test("이야");
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
